a6608ab76939f02897f045c2f0d22d6a62f397be
ljy2855 <ljy2855@naver.com>
2025-03-18 17:38:19 +0900

    change publish config

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Document",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
