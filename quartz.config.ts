af42eb74c22696b59bfa71f7238d2c1d5f2bf752
ljy2855 <ljy2855@naver.com>
2025-03-18 17:39:31 +0900

    update config

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://ljy2855.github.io/obsidian.md/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
